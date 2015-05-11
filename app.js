2f8ae9a4267ea674c03c8074b8895c182f070267
Chris Rinaldi <cgrinaldi@gmail.com>
2015-05-11 15:45:51 -0400

    Schedule multiple messages deterministically

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ var twilio = require('twilio');
 var client = twilio(config.ACCOUNT_SID, config.AUTH_TOKEN);
 var cronJob = require('cron').CronJob;
 
+// Create Stoic reminder messages
+var thoughtReminders = [
+  'What is the worst thing that could have happened to you at this point in the day?',
+  'Where could you be working right now that is much worse than your current job?',
+  'Imagine if a living loved one all of a sudden died. How would you feel?',
+  'Are you dreaming right now? How do you know?'
+];
+
+var cronTimes = [
+  '44 15 * * *',
+  '44 15 * * *',
+  '45 15 * * *',
+  '45 15 * * *'
+];
+
 // `scheduleText()` will create a cronJob that will send the input text
 // at the desired time.
 var scheduleText = function(cronTime, message) {
@@ -18,6 +33,6 @@ var scheduleText = function(cronTime, message) {
   }, null, true);
 };
 
-scheduleText('38 15 * * *', 'Why hello!');
-
-
+cronTimes.forEach(function(time, i){
+  scheduleText(time, thoughtReminders[i]);
+});
