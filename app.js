bccc163f87d65beeabe2c3013bdf3ba8fcee8e37
Chris Rinaldi <cgrinaldi@gmail.com>
2015-05-11 15:38:43 -0400

    Create scheduleText() to allow for sending of message at particular time

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ var twilio = require('twilio');
 var client = twilio(config.ACCOUNT_SID, config.AUTH_TOKEN);
 var cronJob = require('cron').CronJob;
 
-// Schedule a SMS to be sent
-var textJob = new cronJob('27 15 * * *', function() {
-  client.sendMessage({
-    to: config.myPhoneNumber,
-    from: config.twilioPhoneNumber,
-    body: 'Hello, World! Hope you are having a great day!'
-  }, function(err, data) {
-    if (err) {
-      console.error('error is', err);
-      console.log('data is', data);
-    }
-  });
-}, null, true);
+// `scheduleText()` will create a cronJob that will send the input text
+// at the desired time.
+var scheduleText = function(cronTime, message) {
+  new cronJob(cronTime, function() {
+    client.sendMessage({
+      to: config.myPhoneNumber,
+      from: config.twilioPhoneNumber,
+      body: message
+    }, function(err, data){});
+  }, null, true);
+};
+
+scheduleText('38 15 * * *', 'Why hello!');
+
+
