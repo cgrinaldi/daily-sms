1b19ede3bf23336c709613a4eb9964cc2ff755a8
Chris Rinaldi <cgrinaldi@gmail.com>
2015-05-11 15:27:21 -0400

    Send SMS using Twilio and a cron job

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ var config = require('./config.js');
 var twilio = require('twilio');
 var client = twilio(config.ACCOUNT_SID, config.AUTH_TOKEN);
 var cronJob = require('cron').CronJob;
+
+// Schedule a SMS to be sent
+var textJob = new cronJob('27 15 * * *', function() {
+  client.sendMessage({
+    to: config.myPhoneNumber,
+    from: config.twilioPhoneNumber,
+    body: 'Hello, World! Hope you are having a great day!'
+  }, function(err, data) {
+    if (err) {
+      console.error('error is', err);
+      console.log('data is', data);
+    }
+  });
+}, null, true);
