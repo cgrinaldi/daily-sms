bd321692f83087e096d1b97d1308941549585770
Chris Rinaldi <cgrinaldi@gmail.com>
2015-05-13 14:36:28 -0400

    (fix) Remove reminders.js from gitignore

---
+++ reminders.js
@@ -0,0 +1,20 @@
+// Stoic reminder messages
+module.exports.stoicReminders = [
+  'What is the worst thing that could have happened to you at this point in the day?',
+  'Where could you be working right now that is much worse than your current job?',
+  'Imagine if a living loved one all of a sudden died. How would you feel?',
+  'Imagine what your life would be like if you lost your sense of sight.',
+  'Image what your life would be like if you were homeless and living on the streets in the dead of winter.',
+  'Remember when you say goodbye to a friend, it could be the last time that you see them.',
+  'What if today was your last day to live?',
+  'What would your life be like if all of your financial accounts were emptied?',
+  'Imagine what your life would be like if you were paralyzed from the neck down.',
+  'What would your work place be like if you had the world\'s worst boss?',
+  'How could your current job be worse?',
+  'Imagine that WWIII broke out, and you were drafted to fight. How would you feel?',
+  'Imagine what your life would be like if you did\'t have access to all of the technology that you do today.'
+];
+
+module.exports.dreamingReminders = [
+  'Are you dreaming right now? How do you know?'
+];
