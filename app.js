d3e9fe1f498484c9b369a203927e8cd0d2914071
Chris Rinaldi <cgrinaldi@gmail.com>
2015-05-13 14:08:17 -0400

    Refactor to move reminders to separate module

--- app.js
+++ app.js
@@ -12,26 +12,7 @@ var AUTH_TOKEN = config.AUTH_TOKEN || process.env.AUTH_TOKEN;
 var twilio = require('twilio');
 var client = twilio(ACCOUNT_SID, AUTH_TOKEN);
 var cronJob = require('cron').CronJob;
-
-// Create Stoic reminder messages
-var stoicReminders = [
-  'What is the worst thing that could have happened to you at this point in the day?',
-  'Where could you be working right now that is much worse than your current job?',
-  'Imagine if a living loved one all of a sudden died. How would you feel?',
-  'Imagine what your life would be like if you lost your sense of sight.',
-  'Image what your life would be like if you were homeless and living on the streets in the dead of winter.',
-  'Remember when you say goodbye to a friend, it could be the last time that you see them.',
-  'What if today was your last day to live?',
-  'What would your life be like if all of your financial accounts were emptied?',
-  'Imagine what your life would be like if you were paralyzed from the neck down.',
-  'What would your work place be like if you had the world\'s worst boss?',
-  'How could your current job be worse?',
-  'Imagine that WWIII broke out, and you were drafted to fight. How would you feel?'
-];
-
-var dreamingReminders = [
-  'Are you dreaming right now? How do you know?'
-];
+var reminders = require('./reminders.js');
 
 // `scheduleText()` will create a cronJob that will send the input text
 // at the desired time.
@@ -88,8 +69,8 @@ var scheduleMessages = function(n, messages) {
 
 // Schedule messages to be sent. This runs once per day, automatically,
 // with Heroku.
-console.log(scheduleMessages(2, stoicReminders));
-console.log(scheduleMessages(2, dreamingReminders));
+console.log(scheduleMessages(2, reminders.stoicReminders));
+console.log(scheduleMessages(2, reminders.dreamingReminders));
 
 // Seeing what the server time is.
 console.log('server time is:', new Date());
---
+++ reminders_example.js
@@ -0,0 +1,10 @@
+// Reminder messages
+module.exports.stoicReminders = [
+  'ENTER A REMINDER HERE',
+  'ENTER A REMINDER HERE'
+];
+
+module.exports.dreamingReminders = [
+  'ENTER A REMINDER HERE',
+  'ENTER A REMINDER HERE'
+];
