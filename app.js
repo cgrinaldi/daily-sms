747289b322f46c758550da3699f31d9568b624a3
Chris Rinaldi <cgrinaldi@gmail.com>
2015-05-13 13:58:30 -0400

    Add ability to display different messages at varying frequencies

--- app.js
+++ app.js
@@ -14,10 +14,22 @@ var client = twilio(ACCOUNT_SID, AUTH_TOKEN);
 var cronJob = require('cron').CronJob;
 
 // Create Stoic reminder messages
-var thoughtReminders = [
+var stoicReminders = [
   'What is the worst thing that could have happened to you at this point in the day?',
   'Where could you be working right now that is much worse than your current job?',
   'Imagine if a living loved one all of a sudden died. How would you feel?',
+  'Imagine what your life would be like if you lost your sense of sight.',
+  'Image what your life would be like if you were homeless and living on the streets in the dead of winter.',
+  'Remember when you say goodbye to a friend, it could be the last time that you see them.',
+  'What if today was your last day to live?',
+  'What would your life be like if all of your financial accounts were emptied?',
+  'Imagine what your life would be like if you were paralyzed from the neck down.',
+  'What would your work place be like if you had the world\'s worst boss?',
+  'How could your current job be worse?',
+  'Imagine that WWIII broke out, and you were drafted to fight. How would you feel?'
+];
+
+var dreamingReminders = [
   'Are you dreaming right now? How do you know?'
 ];
 
@@ -60,10 +72,10 @@ var sortCronTimes = function(a, b){
 };
 
 // Schedule n different messages throughout the day.
-var scheduleMessages = function(n) {
+var scheduleMessages = function(n, messages) {
   var times = [];
   for (var i = 0; i < n; i++) {
-    var message = randomMessage(thoughtReminders);
+    var message = randomMessage(messages);
     var time = randomTime(8, 20);
     scheduleText(time, message);
     times.push(time);
@@ -74,8 +86,10 @@ var scheduleMessages = function(n) {
   return times;
 };
 
-// Schedule messages to be sent.
-console.log(scheduleMessages(8));
+// Schedule messages to be sent. This runs once per day, automatically,
+// with Heroku.
+console.log(scheduleMessages(2, stoicReminders));
+console.log(scheduleMessages(2, dreamingReminders));
 
 // Seeing what the server time is.
 console.log('server time is:', new Date());
